04ce0766c0ae03d59e4c263c00b713a0c99161ca
xeegeesc <x.egea.es@gmail.com>
2021-12-05 12:32:16 +0100

    clase para dosis

---
+++ app/src/main/java/com/example/loginultimodia/objetoDosis.java
@@ -0,0 +1,60 @@
+package com.example.loginultimodia;
+
+import java.util.Date;
+
+public class objetoDosis {
+
+    private String Medicamento;
+    private String NumPastillas;
+    private Date HoraFecha;
+    private int Frecuencia;
+
+
+    //-------------------------CONSTRUCTOR-----------------------------
+
+    public objetoDosis(String med, String num, int freq){
+            this.Medicamento = med;
+            this.NumPastillas = num;
+            this.HoraFecha = new Date();
+            this.Frecuencia = freq;
+    }
+
+    //-------------------------GETTERS---------------------------------
+
+    public String getMedicamento() {
+        return Medicamento;
+    }
+
+    public String getNumPastillas() {
+        return NumPastillas;
+    }
+
+    public Date getHoraFecha() {
+        return HoraFecha;
+    }
+
+    public int getFrecuencia() {
+        return Frecuencia;
+    }
+
+
+    //-------------------------SETTERS---------------------------------
+
+    public void setMedicamento(String medicamento) {
+        Medicamento = medicamento;
+    }
+
+    public void setNumPastillas(String numPastillas) {
+        NumPastillas = numPastillas;
+    }
+
+    public void setHoraFecha(Date horaFecha) {
+        HoraFecha = horaFecha;
+    }
+
+    public void setFrecuencia(int frecuencia) {
+        Frecuencia = frecuencia;
+    }
+
+
+}
