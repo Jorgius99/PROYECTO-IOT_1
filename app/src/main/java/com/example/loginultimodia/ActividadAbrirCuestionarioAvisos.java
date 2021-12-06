194fd2e20305ee7c544598fb1dc40faa783a114a
alvaroescr0008 <72156197+alvaroescr0008@users.noreply.github.com>
2021-12-06 17:28:30 +0100

    res

--- app/src/main/java/com/example/loginultimodia/ActividadAbrirCuestionarioAvisos.java
+++ app/src/main/java/com/example/loginultimodia/ActividadAbrirCuestionarioAvisos.java
@@ -9,7 +9,7 @@ public class ActividadAbrirCuestionarioAvisos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cuestionario_avisos);
-
+//
 
     }
 }
